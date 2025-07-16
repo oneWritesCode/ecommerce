c6fa10db4aeed43adbf597d1232db8de62460b67
Deepaksingh126 <lalmohansingh83@gmail.com>
2025-07-16 12:40:03 +0530

    added text route

--- backend/index.js
+++ backend/index.js
@@ -15,8 +15,8 @@ ConnectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173", 
-  credentials: true, 
+  origin: "http://localhost:5173",
+  credentials: true,
 }));
 
 // Serve static files from public/uploads folder
@@ -25,6 +25,13 @@ app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 app.use("/user", userRouter);
 app.use("/api", productRouter);
 app.use("/cart", cartRouter);
+
+app.get('/', (req, res) => {
+  res.json({ message: "available get routes : /api/all-products" })
+})
+
+
+
 
 app.listen(8000, () => {
   console.log("server connected succesfully");
